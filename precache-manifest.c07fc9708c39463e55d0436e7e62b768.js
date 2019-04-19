1e4bd09be1d21a4b6d3aa2a74216c3dbe441317d
Ethan Li <ethanli1983@gmail.com>
2019-04-20 08:51:59 +1000

    fix border

---
+++ precache-manifest.c07fc9708c39463e55d0436e7e62b768.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5168313e2b69057a966e",
+    "url": "/static/js/main.5168313e.chunk.js"
+  },
+  {
+    "revision": "1686d66c95e042ffc313",
+    "url": "/static/js/2.1686d66c.chunk.js"
+  },
+  {
+    "revision": "5168313e2b69057a966e",
+    "url": "/static/css/main.2186ee9c.chunk.css"
+  },
+  {
+    "revision": "25bdf90447ecfc8345988d23a9479482",
+    "url": "/index.html"
+  }
+];
